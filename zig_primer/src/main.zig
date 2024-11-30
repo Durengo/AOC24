3dc57c5c02d17985414bb82c71be59e1301af53c
Sebastian <Sebastian.Termen@gmail.com>
2024-11-30 17:49:00 +0200

    zig primer

---
+++ zig_primer/src/main.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const print = std.log.info;
+    print("Hello, {s}!", .{"world"});
+
+    const x: i32 = 1;
+    const pointer: *const i32 = &x;
+    print("1 = {}, {}\n", .{ x, pointer.* });
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    std.debug.print("list length: {d}\n", .{list.items.len});
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "learn" {
+    const mat4x4 = [4][4]f32{
+        [_]f32{ 1.0, 0.0, 0.0, 0.0 },
+        [_]f32{ 0.0, 1.0, 0.0, 0.0 },
+        [_]f32{ 0.0, 0.0, 1.0, 0.0 },
+        [_]f32{ 0.0, 0.0, 0.0, 1.0 },
+    };
+
+    var i: usize = 0;
+    for (mat4x4) |row| {
+        var j: usize = 0;
+        for (row) |cell| {
+            std.debug.print("mat4x4[{d}][{d}] = {d:.1}\n", .{ i, j, cell });
+            j += 1;
+        }
+        i += 1;
+    }
+
+    try std.testing.expect(mat4x4[1][1] == 1.0);
+}
